e6a1eb3233ae85eb7bb82959c0392cfe0a41a369
mineyev2 <mineyev2@illinois.edu>
2020-07-17 19:14:02 -0500

    changed the image resolution

--- python video stream/client.py
+++ python video stream/client.py
@@ -13,7 +13,8 @@ connection = client_socket.makefile('wb')
 try:
     camera = picamera.PiCamera()
     camera.vflip = True
-    camera.resolution = (500, 480)
+    # used to be (500, 480)
+    camera.resolution = (100, 96)
     # Start a preview and let the camera warm up for 2 seconds
     camera.start_preview()
     time.sleep(2)
