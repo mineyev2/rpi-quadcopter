21cf2e3a3f1203cbd42276ca47a658e08d62f011
mineyev2 <mineyev2@illinois.edu>
2020-07-26 20:51:58 -0500

    changed port number on test

--- Computer Controller/client_test.py
+++ Computer Controller/client_test.py
@@ -5,7 +5,7 @@ import socket
 s = socket.socket()
 
 # Define the port on which you want to connect
-port = 12345
+port = 12346
 
 # connect to the server on local computer
 s.connect(('192.168.1.159', port))
