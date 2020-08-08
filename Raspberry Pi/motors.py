101be746eb4b252e46e5f1b438bbddd50efcc946
mineyev2 <mineyev2@illinois.edu>
2020-08-08 00:05:52 -0500

    added comments

--- Raspberry Pi/motors.py
+++ Raspberry Pi/motors.py
@@ -1,6 +1,11 @@
 import pigpio
 import time
 
+'''
+BLHeli firmware settings:
+https://github.com/4712/BLHeliSuite/blob/master/Manuals/BLHeli_S%20manual%20SiLabs%20Rev16.x.pdf
+'''
+
 class Motors:
     def __init__(self):
         self.motor_gpios = [17, 27, 22, 23]
@@ -12,50 +17,84 @@ class Motors:
             self.pi.set_mode(i, pigpio.OUTPUT)
 
     def initialize(self):
-        print("Initializing motors...")
-        for i in self.motor_gpios:
-            self.pi.set_servo_pulsewidth(i, 0)
-        time.sleep(3)
+        '''
+        Initializes the ESCs. Without sending set pulse-width signals to the ESCs, the motors will not run
+        After running initializations, then the motors can be set to any speed we desire
+        :return:
+        '''
 
+        print("Initializing motors...")
+
+        # sequence of PWMs required for BLHeli firmware to "unlock" the ESCs
         for i in self.motor_gpios:
             self.pi.set_servo_pulsewidth(i, 1000)
-        time.sleep(10)
+        time.sleep(5)
         for i in self.motor_gpios:
-            self.pi.set_servo_pulsewidth(i, 0)
+            self.pi.set_servo_pulsewidth(i, 1300)
+        time.sleep(2)
+        for i in self.motor_gpios:
+            self.pi.set_servo_pulsewidth(i, 1000)
+        time.sleep(5)
         print("Motors initialized!")
 
         #listen for beeping to guarantee that the ESCs have initialized
 
+    def calibrate(self, min, max):
+        '''
+        Calibrates ESCs and sets the minimum and maximum pulse-widths that will be sent over from the raspberry pi
+        Will usually set min=1000, max=2000 as default, so not exactly necessary here
+        (see the github link at the top for more info on BLHeli firmware configuation)s
+
+        :param min: minimum throttle pulse-width
+        :param max: maximum throttle pulse-width
+        :return:
+        '''
+
+        print("Calibrating motors...")
+        # TODO: check if calibration sequence is working
+
+        for i in self.motor_gpios:
+            self.pi.set_servo_pulsewidth(i, 1000)
+        time.sleep(5)
+        for i in self.motor_gpios:
+            self.pi.set_servo_pulsewidth(i, max)
+        time.sleep(5)
+        for i in self.motor_gpios:
+            self.pi.set_servo_pulsewidth(i, min)
+        time.sleep(5)
+        print("Motors calibrated!")
+
+
+
+
     def set_speeds(self, speeds):
         '''
         function to set speeds of each motor
+        Will be called each time there is an update in any of the 3 motor speeds
 
         :param speeds: array of speeds, element values range from 0-100 (to symbolize percentages of full power)
-        :return: nothing
+        :return:
         '''
-        #speeds list must be the same size as the number of motors
+
+        # TODO: if only changing one of the motor speeds instead of all 4, this might be inefficient.
+        # TODO: Should look into whether there is a way to simplify this or not
+
+        #speeds list must be the same length as the number of motors
         assert(len(speeds) == self.num_motors), "speeds array contains wrong number of elements"
 
 
         for i in range(self.num_motors):
-            #convert from percentage to signal PWM
+            #convert from motor speed percentage to signal PWM
             # 1000 is min thrust, 2000 is max thrust
-            if(speeds[i] != 0):
-                pulse_width = 1000 + int(speeds[i] * 10)
-                assert(pulse_width <= 2000), "pulse width is too large!"
-                self.pi.set_servo_pulsewidth(self.motor_gpios[i], pulse_width)
-            else:
-                self.pi.set_servo_pulsewidth(self.motor_gpios[i], 0)
+            pulse_width = 1000 + int(speeds[i] * 10)
+            assert(pulse_width <= 2000), "pulse width is too large!"
+            self.pi.set_servo_pulsewidth(self.motor_gpios[i], pulse_width)
 
 
     def turn_off(self):
         '''
         turns off the motors
-        :return: nothing
+        :return:
         '''
         for i in self.motor_gpios:
             self.pi.set_servo_pulsewidth(i, 0)
-
-    def calibrate(self):
-        # need to figure out how to calibrate eventually, keeping blank for now
-        return
